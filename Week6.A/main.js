bd4c18870c59832d18f541c800a5937351256078
cornelbeard <120396011+cornelbeard@users.noreply.github.com>
2023-04-23 14:16:14 -0500

    Add files via upload

---
+++ Week6.A/main.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+      facts: [
+          "Llamas are domesticated South American camelids",
+          "They are used as pack animals and as source of meat, fiber and fuel",
+          "Their wool is soft and lanolin-free, making it hypoallergenic",
+          "Llamas have three stomach compartments",
+          "They are social animals and prefer to live in herds",
+          "Llamas communicate through body language, humming and spitting",
+          "Their gestation period is about 11 months",
+          "Llamas can live up to 20 years",
+          "They are good swimmers and can cross rivers and streams",
+          "Llamas are curious and intelligent animals"
+      ],
+      showFact: true
+  }
+});
