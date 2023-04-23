70178e54f71c6707e17d1269357042c9c9c0bee6
cornelbeard <120396011+cornelbeard@users.noreply.github.com>
2023-04-23 14:44:19 -0500

    Add files via upload

---
+++ Week7/Week7.1/Week7.2/app.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    llamas: ['Pablo', 'Luis', 'Pepe', 'Carmen', 'Llama Del Rey', 'Llama Llama', 'Tina Llama', 'Drama Llama', 'Llama Chameleon', 'Barack O Llama'],
+    llamaLink: 'https://en.wikipedia.org/wiki/Llama',
+    alpacaLink: 'https://en.wikipedia.org/wiki/Alpaca'
+  }
+});
